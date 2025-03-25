2c304f557616fdc57e7cd15e09baac3b6e55e725
SEBASBELMOS <sebasbelmosxd@gmail.com>
2025-03-25 16:43:15 -0500

    src
    Co-Authored-By: XGabrielEdwardsX <144510588+XGabrielEdwardsX@users.noreply.github.com>

--- src/database/connection.py
+++ src/database/connection.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 
-# Reading the environment variables
 load_dotenv("./env/.env")
 
 driver = os.getenv("PG_DRIVER")
@@ -17,7 +16,6 @@ port = os.getenv("PG_PORT")
 
 database = os.getenv("PG_DATABASE")
 
-# Creating the connection engine from the URL made up of the environment variables
 def creating_engine():
     url = f"{driver}://{user}:{password}@{host}:{port}/{database}"
     engine = create_engine(url)
